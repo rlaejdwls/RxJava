cd600047cdf7c5ae669f093f039e6520d167e635
uitaek <uitaek@tigrison.com>
2018-01-12 12:49:24 +0900

    RxJava - Basic

--- src/main/java/com/example/rxjava/RxJavaBasic.java
+++ src/main/java/com/example/rxjava/RxJavaBasic.java
@@ -1,13 +1,26 @@
 package com.example.rxjava;
 
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 public class RxJavaBasic {
-	public void emit() {
-		Observable.just("Hello", "Rx World")
-		.subscribe(System.out::println);
+	public void just() {
+		Disposable disposable = Observable.just("Hello", "Rx World")
+		.subscribe(
+				(data) -> System.out.println(data),
+				(error) -> System.out.println("onError"),
+				() -> System.out.println("onComplete"));
+		System.out.println(disposable.isDisposed());
+	}
+	public void create() {
+		Observable.create((emitter) -> {
+			emitter.onNext("Hello");
+			emitter.onNext("Rx World");
+			emitter.onComplete();
+		}).subscribe(System.out::println);
 	}
 	public static void main(String[] args) {
-		new RxJavaBasic().emit();
+		new RxJavaBasic().just();
+		new RxJavaBasic().create();
 	}
 }
